5743c09bfd5f67d262294101a19f7768118f6e94
DeveloperKhurram <developerkhurram007@gmail.com>
2024-09-20 14:20:33 +0500

    spinner removed

--- js/spinner.js
+++
@@ -1,8 +0,0 @@
-// Wait for the entire page to load
-window.onload = function () {
-  // Hide the spinner after 3 seconds (3000 milliseconds)
-  setTimeout(function () {
-    document.getElementById("spinner").style.display = "none"; // Hide the spinner
-    document.getElementById("content").style.display = "block"; // Show the content
-  }, 2000);
-};
